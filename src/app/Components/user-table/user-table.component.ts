16a9470bdcf78cd63f3b2bd5900d62c4bd94f6f3
Suranjana Roy <roysupra75@gmail.com>
2025-02-02 12:03:42 +0530

    added pagination

--- src/app/Components/user-table/user-table.component.ts
+++ src/app/Components/user-table/user-table.component.ts
@@ -12,21 +12,39 @@ export class UserTableComponent implements OnInit{
   userList: any[] = [];
   workoutOptions: string[] = ['Running', 'Cycling', 'Yoga', 'Swimming', 'Weightlifting'];
   filter: string = '';
+
+  currentPage: number = 1;
+  itemsPerPage: number = 5; 
+  totalItems: number = 0;
+
   constructor(private userService: UserService){}
   ngOnInit(): void {
     this.userService.userList$.subscribe((userList)=>{
       this.userList = userList;
-      console.log(userList)
+      this.totalItems = this.userList.length; 
     })
   }
   get filteredUsers() {
-    if (this.filter === "") { // Check for empty string instead of "All"
-      return this.userList;
+    let users = this.userList;
+    if (this.filter === "") { 
+      users = this.userList;
     }
     if (this.filter) {
-      return this.userList.filter(user => user.workoutType.includes(this.filter));
+      users = this.userList.filter(user => user.workoutType.includes(this.filter));
     }
-    return this.userList;
+    // return this.userList;
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    return users.slice(startIndex, startIndex + this.itemsPerPage);
   }
-  }
+
+    // Method to change page
+    changePage(page: number) {
+      this.currentPage = page;
+    }
+
+    get totalPages() {
+      return Math.ceil(this.totalItems / this.itemsPerPage);
+    }
+
+}
 
