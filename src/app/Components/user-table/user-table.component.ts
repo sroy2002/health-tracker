8e61602e04bf696edf08f321b77a3be527bb497a
Suranjana Roy <roysupra75@gmail.com>
2025-02-02 12:50:28 +0530

    added search functionality

--- src/app/Components/user-table/user-table.component.ts
+++ src/app/Components/user-table/user-table.component.ts
@@ -1,50 +1,73 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../user.service';
 @Component({
   selector: 'app-user-table',
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './user-table.component.html',
-  styleUrl: './user-table.component.css'
+  styleUrl: './user-table.component.css',
 })
-export class UserTableComponent implements OnInit{
+export class UserTableComponent implements OnInit {
   userList: any[] = [];
-  workoutOptions: string[] = ['Running', 'Cycling', 'Yoga', 'Swimming', 'Weightlifting'];
+  workoutOptions: string[] = [
+    'Running',
+    'Cycling',
+    'Yoga',
+    'Swimming',
+    'Weightlifting',
+  ];
   filter: string = '';
 
   currentPage: number = 1;
-  itemsPerPage: number = 5; 
+  itemsPerPage: number = 5;
   totalItems: number = 0;
+  itemsPerPageOptions: number[] = [5, 10, 20];
+  searchTerm: string='';
 
-  constructor(private userService: UserService){}
+  constructor(private userService: UserService) {}
   ngOnInit(): void {
-    this.userService.userList$.subscribe((userList)=>{
+    this.userService.userList$.subscribe((userList) => {
       this.userList = userList;
-      this.totalItems = this.userList.length; 
-    })
+      this.totalItems = this.userList.length;
+    });
   }
+
+
   get filteredUsers() {
     let users = this.userList;
-    if (this.filter === "") { 
+    if (this.filter === '') {
       users = this.userList;
     }
     if (this.filter) {
-      users = this.userList.filter(user => user.workoutType.includes(this.filter));
+      users = this.userList.filter((user) =>
+        user.workoutType.includes(this.filter)
+      );
     }
-    // return this.userList;
+
+    
+  if (this.searchTerm) {
+    users = users.filter(user => user.userName.toLowerCase().includes(this.searchTerm.toLowerCase())); 
+  }
+
+
+
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     return users.slice(startIndex, startIndex + this.itemsPerPage);
   }
 
-    // Method to change page
-    changePage(page: number) {
-      this.currentPage = page;
-    }
+  // Method to change page
+  changePage(page: number) {
+    this.currentPage = page;
+  }
 
-    get totalPages() {
-      return Math.ceil(this.totalItems / this.itemsPerPage);
-    }
+  get totalPages() {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
 
+  // Method to change items per page
+  changeItemsPerPage(event: any) {
+    this.itemsPerPage = +event.target.value; 
+    this.currentPage = 1; 
+  }
 }
-
